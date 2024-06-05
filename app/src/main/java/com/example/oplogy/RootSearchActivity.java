4e65216874c5e44fe464b69523e635418192cee2
it232115 <it232115@trident.ac.jp>
2024-06-05 16:24:29 +0900

    使用するActivityの事前作成

---
+++ app/src/main/java/com/example/oplogy/RootSearchActivity.java
@@ -0,0 +1,14 @@
+package com.example.oplogy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RootSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_root_search);
+    }
+}
