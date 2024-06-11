2d17a13d3bf227f8edebb524371550ae506b0405
Ekamo-11 <148836598+Ekamo-11@users.noreply.github.com>
2024-06-11 16:53:13 +0900

    開始時刻と、終了時刻をクリックすると選択した時間が挿入される機能を作成しました。ただ両方の時刻とも挿入されるので、修正します

--- app/src/main/java/com/example/oplogy/SetUpActivity.java
+++ app/src/main/java/com/example/oplogy/SetUpActivity.java
@@ -1,14 +1,43 @@
 package com.example.oplogy;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.DialogFragment;
 import android.os.Bundle;
+import android.app.TimePickerDialog;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import java.util.Locale;
 
-public class SetUpActivity extends AppCompatActivity {
+
+public class SetUpActivity extends FragmentActivity
+        implements TimePickerDialog.OnTimeSetListener {
+
+    private TextView startTime;
+    private TextView endTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_up);
+
+        startTime = findViewById(R.id.startTime);
+        endTime = findViewById(R.id.endTime);
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+        String str = String.format(Locale.US, "%d:%d", hourOfDay, minute);
+
+        startTime.setText(str);
+        endTime.setText(str);
+
+    }
+
+    public void showTimePickerDialog(View v) {
+        DialogFragment newFragment = new TimePick();
+        newFragment.show(getSupportFragmentManager(), "timePicker");
+
     }
 }
