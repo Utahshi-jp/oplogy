38a565c3923a9f5aa365890bd04733ec1ff0514b
it232115 <it232115@trident.ac.jp>
2024-06-18 17:20:34 +0900

    giteaへの変更のためのコミット

--- app/src/main/java/com/example/oplogy/CreateRoot.java
+++ app/src/main/java/com/example/oplogy/CreateRoot.java
@@ -1,5 +1,29 @@
 package com.example.oplogy;
 
-public class CreateRoot {
+import android.util.Log;
 
+import com.google.android.gms.maps.model.LatLng;
+import com.google.firebase.Timestamp;
+
+public class CreateRoot {
+    private String address;
+    private Timestamp startTimestamp;
+    private Timestamp endTimestamp;
+    private long studentNumber;
+    private LatLng latLng;
+
+    public void receiveData(String address, Timestamp startTimestamp, Timestamp endTimestamp, long studentNumber, LatLng latLng) {
+        this.address = address;
+        this.startTimestamp = startTimestamp;
+        this.endTimestamp = endTimestamp;
+        this.studentNumber = studentNumber;
+        this.latLng = latLng;
+
+        // 受け取ったデータを利用してログを出力
+        Log.d("CreateRoot", "address: " + address);
+        Log.d("CreateRoot", "startTimestamp: " + startTimestamp);
+        Log.d("CreateRoot", "endTimestamp: " + endTimestamp);
+        Log.d("CreateRoot", "studentNumber: " + studentNumber);
+        Log.d("CreateRoot", "latLng: " + latLng);
+    }
 }
