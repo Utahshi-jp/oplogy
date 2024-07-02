9dec1476e108f5f126a42099cb209a1f33f6e2c4
oKikuti <fureiazumajp@gmail.com>
2024-07-02 20:56:34 +0900

    菊池
    名前の追加（scrollView）に成功。今度は名前と緯度経度別々にして、問題なくマップが見れるようにする

--- app/src/main/java/com/example/oplogy/Maps.java
+++ app/src/main/java/com/example/oplogy/Maps.java
@@ -7,6 +7,8 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.fragment.app.FragmentActivity;
 
@@ -15,6 +17,7 @@ import com.example.oplogy.databinding.MapsBinding;
 public class Maps extends FragmentActivity implements View.OnClickListener {
 
     private WebView webView;
+    private LinearLayout locationsName;
     ImageView backMain;
     private MapsBinding binding;
 
@@ -29,20 +32,29 @@ public class Maps extends FragmentActivity implements View.OnClickListener {
         backMain.setOnClickListener(this);
 
         webView = findViewById(R.id.webView);
+        locationsName = findViewById(R.id.locationsName);
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webView.setWebViewClient(new WebViewClient());
 
-//        ここにデータを入れておいてください、処理は[/]で区切っています(緯度と経度で入れているので、そこで一度試してください
-        loadMapInWebView("35.09050879999539,136.87845379325216/35.091950716938875, 136.8826598363985/35.09273643623442, 136.88154941341296");
+        // ここにデータを入れておいてください、処理は[/]で区切っています(緯度と経度で入れているので、そこで一度試してください)
+        loadMapInWebView("35.09050879999539,136.87845379325216,名古屋港水族館/35.091950716938875,136.8826598363985,2番目/35.09273643623442,136.88154941341296,3番目");
     }
 
-//    WebViewの処理です（Mapの中の処理をやっています）
+    // WebViewの処理です（Mapの中の処理をやっています）
     private void loadMapInWebView(String locations) {
-//        区切ることで、追加の地点を入れて、最終地点にピンを打ってある状態です
+        // 区切ることで、追加の地点を入れて、最終地点にピンを打ってある状態です
         String[] locArray = locations.split("/");
 
-//        ↓URLで経路案内（車）での表示をしています
+        // 各地点の名前をScrollViewに表示
+        for (String loc : locArray) {
+            String[] parts = loc.split(",");
+            if (parts.length == 3) {
+                addLocationToScrollView(parts[2]);
+            }
+        }
+
+        // URLで経路案内（車）での表示をしています
         StringBuilder urlBuilder = new StringBuilder("https://www.google.com/maps/dir/?api=1&travelmode=driving");
 
         if (locArray.length > 0) {
@@ -64,6 +76,14 @@ public class Maps extends FragmentActivity implements View.OnClickListener {
         }
 
         webView.loadUrl(urlBuilder.toString());
+    }
+
+    private void addLocationToScrollView(String locationName) {
+        TextView textView = new TextView(this);
+        textView.setText(locationName);
+        textView.setTextSize(16);
+        textView.setPadding(16, 16, 16, 16);
+        locationsName.addView(textView);
     }
 
     @Override
