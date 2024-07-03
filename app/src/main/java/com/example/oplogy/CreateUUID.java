e066eb057fb87c7a6ab86ac722cc120a936eae9a
nemukemo <hiroaka201432@gmail.com>
2024-07-03 15:07:31 +0900

    なんか消えてたから戻しました

--- app/src/main/java/com/example/oplogy/CreateUUID.java
+++ app/src/main/java/com/example/oplogy/CreateUUID.java
@@ -5,12 +5,12 @@ import java.util.List;
 
 public class CreateUUID {
 
-    public static int generateUUID(List<String> classIdList ){
+    public static int generateUUID(List<Integer> classIdList ){
         while (true){
-            String uuid = String.valueOf((int)(Math.random() * 1000000));
+            int uuid = (int) (Math.random() * 100000);
             boolean isDuplicate = false;
-            for(String classId : classIdList){
-                if(classId.equals(uuid)){
+            for(int classId : classIdList){
+                if(classId==uuid){
                     //重複があればフラグを立て、ループを抜ける
                     isDuplicate = true;
                     break;
@@ -19,9 +19,11 @@ public class CreateUUID {
             //重複がなければ生成したUUIDを返す
             if (!isDuplicate) {
                 //firestoreに挿入処理
+                InsertClassIdforFirebase insertClassIdforFirebase = new InsertClassIdforFirebase();
+                insertClassIdforFirebase.insertClassId(uuid);
                 //テスト用
-                uuid="100";
-                return Integer.parseInt(uuid);
+                uuid=100;
+                return uuid;
             }
         }
     }
